619000caf76fc58bb112ddb57277a7ecd30d1118
saalehmohamedd <saalehmohamedd@gmail.com>
2026-02-05 16:11:14 +0200

    fix warrinings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', // This allows all images from your Cloudinary account
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
