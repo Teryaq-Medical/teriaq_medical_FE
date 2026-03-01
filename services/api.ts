f20ae02271e833e702b3820ad564cb6b874fa724
saalehmohamedd <saalehmohamedd@gmail.com>
2026-03-01 19:15:40 +0200

    test for production

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // عدّلها حسب backend
+  baseURL: "https://teriaq-medical-be.onrender.com/api", // عدّلها حسب backend
   headers: {
     "Content-Type": "application/json",
   },
